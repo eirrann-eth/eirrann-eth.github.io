200437f06008132effb54a5cf45d76c738f4a620
eirrann.eth <128864253+eirrann-eth@users.noreply.github.com>
2023-03-25 22:56:36 -0400

    updated design and code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Import dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 import './index.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
@@ -14,30 +16,32 @@ import Footer from './components/Footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Define main component
-function App() {
+function Main() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div>
-        <Header />
-          <Routes>
-             <Route path="/" element={<Home />} />
-             <Route path="/about" element={<About />} />
-              <Route path="/gallery" element={<Gallery />} />
-              <Route path="/showcase" element={<Showcase />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/privacy" element={<Privacy />} />
-             <Route path="/terms" element={<Terms />} />
-            </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div>
+      <Header />
+      <main style={{ margin: '0 auto', maxWidth: '960px', padding: '1em' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/showcase" element={<Showcase />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
 // Render main component to DOM
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Main />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root')
 );
